cf0c2b6a0cc6079aaf35c908d4446e698b0fd449
Leo Lai <asd13006@gmail.com>
2026-03-18 23:08:25 +0800

    v2.10.4: 移除dvh/fill-available — 用100%修復iOS standalone底部色塊

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 🔥 黃金法則：每次發布新版本，一定要手動修改這裡的版號！(例如下一次改為 v2.8.5)
-const APP_VERSION = "v2.10.3";
+const APP_VERSION = "v2.10.4";
 const CACHE_NAME = 'mahjong-cache-' + APP_VERSION;
 
 const urlsToCache = [
